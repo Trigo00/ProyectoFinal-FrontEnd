e3b7914d4b6e3674463a14338dc00fc1a88054bd
Trigo00 <trigotex@gmail.com>
2022-09-14 04:01:19 -0300

    Actualizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,62 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { TecnologiasComponent } from './tecnologias/tecnologias.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { LoginComponent } from './login/login.component';
+import { NeweducacionComponent } from './educacion/neweducacion.component';
+import { NuevaExperienciaComponent } from './sobre-mi/experiencia/nueva-experiencia.component';
+import { EditExperienciaComponent } from './sobre-mi/experiencia/edit-experiencia.component';
+import { EditeducacionComponent } from './educacion/editeducacion.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: BienvenidaComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'editexp/:id',
+    component: EditExperienciaComponent
+  },
+  {
+    path: 'nuevaexp',
+    component: NuevaExperienciaComponent
+  },
+  {
+    path: 'nuevaedu',
+    component: NeweducacionComponent
+  },
+  {
+    path: 'editedu/:id',
+    component: EditeducacionComponent
+  },
+  {
+    path: 'proyectos',
+    component: ProyectosComponent
+  },
+  {
+    path: 'sobremi',
+    component: SobreMiComponent
+  },
+  {
+    path: 'tecnologias',
+    component: TecnologiasComponent
+  },
+  {
+    path: 'contacto',
+    component: ContactoComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
+
+import { interceptorProvider } from './service/interceptor-service';
+
 import { AppComponent } from './app.component';
+import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { TecnologiasComponent } from './tecnologias/tecnologias.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { NuevaExperienciaComponent } from './sobre-mi/experiencia/nueva-experiencia.component';
+import { EditExperienciaComponent } from './sobre-mi/experiencia/edit-experiencia.component';
+import { NeweducacionComponent } from './educacion/neweducacion.component';
+import { EditeducacionComponent } from './educacion/editeducacion.component';
+import { EducacionComponent } from './educacion/educacion.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidaComponent,
+    ProyectosComponent,
+    SobreMiComponent,
+    TecnologiasComponent,
+    ContactoComponent,
+    MenuComponent,
+    LoginComponent,
+    NuevaExperienciaComponent,
+    EditExperienciaComponent,
+    NeweducacionComponent,
+    EditeducacionComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
